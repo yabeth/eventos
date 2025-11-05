1ab394e21f1fcde1d588e673cc2b0342bf2df9cc
unknown <yurlin@gmail.com>
2025-11-04 23:50:06 -0500

    Se creo una nueva tabla

--- laravel11/app/Models/evento.php
+++ laravel11/app/Models/evento.php
@@ -6,16 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 use App\Models\EstadoEvento;
 use App\Models\tipoevento;
 use App\Models\Resoluciaprob;
+
 class Evento extends Model
 {
-    
+
     protected $table = 'evento'; // Nombre de la tabla en la base de datos
-    protected $primaryKey = 'idevento'; 
-    protected $fillable= ['eventnom','idTipoeven','idestadoeve','descripción','fecini','horain','horcul']; 
+    protected $primaryKey = 'idevento';
+    protected $fillable = ['eventnom', 'idTipoeven', 'idestadoeve', 'descripción', 'fecini', 'horain', 'horcul'];
     public $timestamps = false; // Si no usas timestamps
     public function estadoEvento()
     {
-        return $this->belongsTo(EstadoEvento::class, 'idestadoeve'); 
+        return $this->belongsTo(EstadoEvento::class, 'idestadoeve');
     }
 
     public function tipoEvento()
@@ -24,7 +25,6 @@ class Evento extends Model
     }
     public function Resoluciaprob()
     {
-        return $this->hasOne(Resoluciaprob::class, 'idevento', 'idevento'); 
+        return $this->hasOne(Resoluciaprob::class, 'idevento', 'idevento');
     }
 }
-
